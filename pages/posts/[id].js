80ff988442b95a5cb8c29efdb6249e92fc5201ea
Kento Iwama <ken10iwm@gmail.com>
2022-02-16 16:37:12 +0900

    og:titleが正しく生成されていなかった問題に対応。レスポンシブ対応を調整。半端なサイズの時にコンテンツが重なる問題に対応。

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -50,9 +50,8 @@ export default function Post({ post, categories, posts }) {
                     <title>{post.title} - MORU&apos;S ORANGE BLOG</title>
                     <meta name="twitter:card" content="summary"></meta>
                     <meta name="twitter:creator" content="@moru3"></meta>
-                    <meta property="og:title" content="`{post.title} - MORU&apos;S ORANGE BLOG`"/>
+                    <meta property="og:title" content={post.title + "- MORU&apos;S ORANGE BLOG"}/>
                     <meta property="og:description" content={post.contentHtml.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '').slice(0, 50) + "...."}/>
-                    <meta property="og:image" content="/apple-touch-icon.png" />
                 </Head>
                 <div className={styles.main}>
                     <div className={styles.title}>{post.title}</div>
